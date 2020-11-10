3cd5abdc87ce8eaf1df1c3433f06ddd54d60baa2
Dehou Kong <124kdaniel@gmail.com>
2020-11-10 12:57:57 -0500

    added skill selection ui and implemented back button functionality

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,12 +12,33 @@ function toggleUI(elementID, className="") {
     }
 }
 
+/**
+ * Set UI visibility
+ * @param {*} on turn the UI visibility on or off
+ * @param {*} elementID id of the element
+ * @param {*} className class name of the element, default is empty
+ */
+function setUI(on, elementID, className="") {
+    var x = document.getElementById(elementID);
+    if (on) {
+        x.className = className;
+    } else {
+        x.className = "d-none"; 
+    }
+}
+
 // JQuery
 $(document).ready(function(){
     $('div#back_button').click(function(){
-        alert("go back!");
+        setUI(false,'skill_select', 'd-flex flex-column')
+        setUI(true,'action_select', 'd-flex flex-column')
+        setUI(false, 'back_button')
     });
     $('div#attack').click(function(){
         alert("attack!");
+    });
+    $('div#skill').click(function(){
+        toggleUI('skill_select', 'd-flex flex-column')
+        toggleUI('back_button')
     });
 });
